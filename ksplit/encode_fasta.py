32e2b6b311a1352d3a272cd7fe10c3750e97927c
Luis Pedro Coelho <luis@luispedro.org>
2021-01-12 15:28:41 +0800

    RFCT Avoid loading the sequences into memory

--- ksplit/encode_fasta.py
+++ ksplit/encode_fasta.py
@@ -26,17 +26,10 @@ def encode_fasta(args, input_file, output_file):
     encode_fastq( { .. }, open(input_file_name, 'rt'), open(output_file_name, 'wb'))
     '''
     
-    #first parse sequences as array from input file using SeqIO
-    fasta_sequences_list = np.array([ seq.seq for seq in SeqIO.parse(input_file, 'fasta') ])
-
-
-    N = len(fasta_sequences_list)
-
     #make list of kmers for each sequence
-    for i in range(N):
-        
+    for i, seq in SeqIO.parse(input_file, 'fasta'):
         #encode sequence, then compute kmers for that sequence
-        ascii_encoded_sequence = fasta_sequences_list[i].encode('ascii') 
+        ascii_encoded_sequence = seq.seq.encode('ascii')
         kmers_array = kmers.kmers( ascii_encoded_sequence )
 
         #checks if current sequence has a character other than ACTG
@@ -62,9 +55,6 @@ def encode_fasta(args, input_file, output_file):
         ixs = np.repeat(np.array([i], dtype=np.uint64), len(kmers_array))
         encoded = np.vstack([kmers_array, ixs]).T.ravel()
         output_file.write(encoded.data)
-        
-        
-        
 
     #number of sequences encoded
     return i+1
